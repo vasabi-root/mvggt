66179ff8cfc9bb1bb2bf316e866ff72b69badf52
bashmac.wsl <vasabi.ph@gmail.com>
2026-02-11 06:51:23 +0300

    Dataset: adjust inputs to img_size from the begining

--- training/dataset.py
+++ training/dataset.py
@@ -113,7 +113,7 @@ def backproject(depth: torch.Tensor, K: torch.Tensor) -> torch.Tensor:
     return torch.stack([x, y, z], dim=-1)  # (H, W, 3)
 
 class ScanReferMvggtDataset(Dataset):
-    def __init__(self, scanrefer_path, scannet_root, tokenizer, num_views=8, img_size=(322, 238)): # TODO: add patch_size and fit img_size with patch_size
+    def __init__(self, scanrefer_path, scannet_root, tokenizer, num_views=8, img_size=(238, 322)): # TODO: add patch_size and fit img_size with patch_size
         '''
         ScanRefer_filtered_train.json not above 0706
         ScanRefer_filtered_val.json not above 0706
@@ -146,16 +146,12 @@ class ScanReferMvggtDataset(Dataset):
         view_list = []
         for fid in frame_ids:
             rgb = self._load_rgb(scene_id, fid)                  # PIL
-            W, H = rgb.size
+            H, W = rgb.shape[-2:]
             
             depth = self._load_depth(scene_id, fid)              # (H W)
             pose_cam2world = self._load_pose(scene_id, fid)
             
-            # K_depth = self._load_intrinsics(scene_id, color=False)   # for backproject
-            K_color, K_depth = self._load_intrinsics(scene_id, (W, H), depth.shape[::-1])
-            # TODO: add intrincsic_color.txt to ScanNet somehow
-            # K_color = self._load_intrinsics(scene_id, color=True)    # for RGB masks 
-            # K_color = K_depth
+            K_color, K_depth = self._load_intrinsics(scene_id)
 
             # backproject depth -> camera coordinates
             pts_local = backproject(depth, K_depth)              # (H W 3)
@@ -166,19 +162,13 @@ class ScanReferMvggtDataset(Dataset):
             # object 3D points -> 2D mask
             obj_pts = self._load_object_points(scene_id, obj_id)
             mask = project_points_to_mask(obj_pts, pose_cam2world, K_color, H, W, True)
-            
-            mask = F.resize(mask.unsqueeze(0), size=self.img_size, antialias=True)
-            depth = F.resize(depth.unsqueeze(0), size=self.img_size, antialias=True)
-
-            # transform RGB. PIL -> Tensor
-            rgb = self.transform(rgb)
 
             view_list.append({
                 'img': rgb,
                 'pts3d': pts_world,
-                'valid_mask': (depth > 0).float(),
+                'valid_mask': depth > 0,
                 'camera_pose': pose_cam2world,
-                'referring_mask': mask
+                'referring_mask': mask.bool()
             })
 
         # text
@@ -203,20 +193,21 @@ class ScanReferMvggtDataset(Dataset):
     def _load_rgb(self, scene_id, frame_idx):
         path = f"{self.scannet_root}/images/{scene_id}/color/{frame_idx}.jpg"      # :06d may be for new scannet
         img = Image.open(path).convert('RGB')
-        # transform (Resize 224, ToTensor, Normalize) further...
+        img = self.transform(img)
         return img
 
     def _load_depth(self, scene_id, frame_idx):
         path = f"{self.scannet_root}/images/{scene_id}/depth/{frame_idx}.png"      # :06d may be for new scannet
         depth = cv2.imread(path, cv2.IMREAD_UNCHANGED).astype(np.float32) / 1000.0
-        return torch.from_numpy(depth).float()  # (H W)
+        depth = torch.from_numpy(depth).float().unsqueeze(0)
+        return F.resize(depth, size=self.img_size, antialias=True).squeeze(0) # (H W)
 
     def _load_pose(self, scene_id, frame_idx):
         path = f"{self.scannet_root}/images/{scene_id}/pose/{frame_idx}.txt"       # :06d may be for new scannet
         pose = np.loadtxt(path).astype(np.float32)  # 4x4 camera-to-world
         return torch.from_numpy(pose).float()
 
-    def _load_intrinsics(self, scene_id, real_color_wh, real_depth_wh):
+    def _load_intrinsics(self, scene_id):
         data = {}
         with open(f"{self.scannet_root}/scans/{scene_id}/{scene_id}.txt") as f:
             for line in f:
@@ -224,8 +215,7 @@ class ScanReferMvggtDataset(Dataset):
                 data[pieces[0]] = pieces[1]
         
         color, depth = "color", "depth"
-        widths  = {color: real_color_wh[0], depth: real_depth_wh[0]}
-        heights = {color: real_color_wh[1], depth: real_depth_wh[1]}
+        H, W = self.img_size
         K = {}
         for key in [color, depth]:
             K[key] = np.zeros((3, 3))
@@ -235,8 +225,8 @@ class ScanReferMvggtDataset(Dataset):
             K[key][1,-1] = float(data[f"my_{key}"])
             
             # correct intrinsics with real height/width of an image
-            K[key][0] *= widths[key]/int(data[f"{key}Width"])
-            K[key][1] *= heights[key]/int(data[f"{key}Height"])
+            K[key][0] *= W/int(data[f"{key}Width"])
+            K[key][1] *= H/int(data[f"{key}Height"])
             
             K[key][-1,-1] = 1
     
@@ -292,8 +282,7 @@ if __name__ == '__main__':
         scanrefer_path=CONF.TRAIN_JSON,
         scannet_root=CONF.PATH.SCANNET,
         tokenizer=tokenizer,
-        num_views=8,
-        img_size=224
+        num_views=8
     )
     dataset[0]
     # dataloader = DataLoader(dataset, batch_size=16, shuffle=True, collate_fn=ScanReferDataset.collate_fn)
